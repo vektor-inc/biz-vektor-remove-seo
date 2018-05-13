2ea08065d3d7c0e00e5b7fed37b43f985a2f0b83
kurudrive <kurudrive@gmail.com>
2018-05-14 01:10:44 +0900

    タイトルのフック構造変更

--- biz-vektor-remove-seo.php
+++ biz-vektor-remove-seo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:     Biz Vektor Remove SEO
+ * Plugin Name:     BizVektor Remove SEO
  * Plugin URI:
  * Description:     BizVektorのタイトル / ディスクリプション / キーワードの出力を停止します
  * Author:          Vektor,Inc.
@@ -12,9 +12,27 @@
  * @package         Biz_Vektor_Remove_Seo
  */
 
+function bv_is_new_version() {
+	$version     = wp_get_theme()->get( 'Version' );
+	$old_version = '1.11.4';
+	if ( version_compare( $version, $old_version ) >= 0 ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 add_action( 'after_setup_theme', 'biz_vektor_remove_seo', 11 );
 function biz_vektor_remove_seo() {
-	remove_theme_support( 'title-tag' );
+	/*
+	1.11.4 以前 function.php の after_setup_theme から add_theme_support
+	1.11.5 以降
+	 */
+	if ( bv_is_new_version() ) {
+		remove_action( 'init', 'biz_vektor_print_title' );
+	} else {
+		remove_theme_support( 'title-tag' );
+	}
 	remove_action( 'wp_head', 'biz_vektor_setHeadDescription', 5 );
 	remove_action( 'wp_head', 'biz_vektor_seo_set_HeadKeywords', 1 );
 }
